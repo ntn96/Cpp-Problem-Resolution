2c7166bc489ac8f8d24ca279ea1899d67aaca369
ntn96 <39365787+ntn96@users.noreply.github.com>
2019-10-06 16:21:48 +0200

    Create AddTwoNumbers.h

---
+++ AddTwoNumbers.h
@@ -0,0 +1,13 @@
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int);
+};
+
+class AddTwoNumbers
+{
+public:
+	static ListNode* addTwoNumbers(ListNode*, ListNode*);
+	AddTwoNumbers();
+	~AddTwoNumbers();
+};
